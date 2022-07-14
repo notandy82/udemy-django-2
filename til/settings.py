cc0ac8937ae802bf70763f85a8ae8cdb4356ca89
notandy82 <notandystanek@gmail.com>
2022-07-14 11:44:16 +0000

    Add media folder for profile images

--- til/settings.py
+++ til/settings.py
@@ -17,6 +17,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_URL = "/media/"
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
